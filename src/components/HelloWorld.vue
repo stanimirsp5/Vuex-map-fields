0094f7046e6a302ed252d73652edbb11d145b122
Stanimir Petrov <stanimir.sp5@gmail.com>
2019-06-22 15:59:31 +0300

    sample

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,119 @@
 <template>
     <div class="hello">
-        <h1>TSTE</h1>
-        <h1>{{ msg }}</h1>
-        <h1>{{ msg }}</h1>
+        <h1>---------------</h1>
+        <h1>{{ mapTestMessage }}</h1>
+        <!-- <input :value="getTestMessage"
+            @input="updateProperty"
+            placeholder="edit me"> -->
+        <input v-model="mapTestMessage"
+            placeholder="edit me">
+        <p>Message is: {{ mapTestMessage }}</p>
+
+        <h1>object ---------------</h1>
+        <input :value="getTestObject.name"
+            @input="updateObject($event, 'name')"
+            placeholder="edit me">
+        <p>Name is: {{ getTestObject.name }}</p>
+        <input :value="getTestObject.age"
+            @input="updateObject($event, 'age')"
+            placeholder="edit me">
+        <p>Age is: {{ getTestObject.age }}</p>
+        <input :value="getTestObject.hobby"
+            @input="updateObject($event, 'hobby')"
+            placeholder="edit me">
+        <p>Hobby is: {{ getTestObject.hobby }}</p>
+
+        <h1>Arrray ---------------</h1>
+        <!-- <div v-for="(item, index) in getTestArray"
+            :key="index">
+            <input :value="item.name"
+                @input="updateArray($event, 'name',index)"
+                placeholder="edit me">
+            <p>Name is: {{ item.name }}</p>
+            <input :value="item.age"
+                @input="updateArray($event, 'age',index)"
+                placeholder="edit me">
+            <p>Age is: {{ item.age }}</p>
+            <input :value="item.hobby"
+                @input="updateArray($event, 'hobby',index)"
+                placeholder="edit me">
+            <p>Hobby is: {{ item.hobby }}</p>
+            <h3>end row-------- </h3>
+
+        </div> -->
+        <br />
+        <br />
+        <br />
+        <h1>With library ---------------</h1>
+
+        <h1>object ---------------</h1>
+        <input v-model="mapTestObject.name"
+            placeholder="edit me">
+        <p>Name is: {{ mapTestObject.name }}</p>
+        <input v-model="mapTestObject.age"
+            placeholder="edit me">
+        <p>Age is: {{ mapTestObject.age }}</p>
+        <input v-model="mapTestObject.hobby"
+            placeholder="edit me">
+        <p>Hobby is: {{ mapTestObject.hobby }}</p>
+
+        <h1>Arrray ---------------</h1>
+        <div v-for="(item2, index2) in mapTestArray"
+            :key="index2">
+            <input v-model="item2.name"
+                placeholder="edit me">
+            <p>Name is: {{ item2.name }}</p>
+            <input v-model="item2.age"
+                placeholder="edit me">
+            <p>Age is: {{ item2.age }}</p>
+            <input v-model="item2.hobby"
+                placeholder="edit me">
+            <p>Hobby is: {{ item2.hobby }}</p>
+            <h3>end row-------- </h3>
+
+        </div>
 
     </div>
 </template>
 
 <script>
+/* eslint-disable */
+
+import { mapGetters } from 'vuex'
+import { mapFields } from 'vuex-map-fields';
+import { mapMultiRowFields } from 'vuex-map-fields';
+
 export default {
     name: 'HelloWorld',
     props: {
-        msg: String
+    },
+    computed: {
+        ...mapGetters([
+            'getTestMessage',
+            'getTestObject',
+            'getTestArray',
+        ]),
+        ...mapFields([
+            'mapTestMessage',
+            'mapTestObject',
+        ]),
+        ...mapMultiRowFields([
+            'mapTestArray',
+        ]),
+    },
+    created() {
+
+    },
+    methods: {
+        updateProperty(text) {
+            this.$store.commit('changeText', text)
+        },
+        updateObject(text, key) {
+            this.$store.commit('changeObject', { key: key, value: text })
+        },
+        updateArray(text, key, index) {
+            this.$store.commit('changeArray', { key: key, value: text, index: index })
+        }
     }
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -1,9 +1,111 @@
+/* eslint-disable */
+import {
+    getField,
+    updateField
+} from 'vuex-map-fields';
 import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
+
 const state = {
-    count: 0
+    testMessage: "testing ,bro",
+    testObject: {
+        name: "Ivan",
+        age: 25,
+        hobby: "Drive cars and travel with the subway"
+    },
+    testArray: [{
+            name: "Ivan",
+            age: 25,
+            hobby: "Drive cars and travel with the subway"
+        },
+        {
+            name: "Joro",
+            age: 21,
+            hobby: "Drink water"
+        },
+        {
+            name: "Mishu",
+            age: 24,
+            hobby: "streaming games"
+        },
+    ],
+
+    mapTestMessage: "Map testing ,bro",
+    mapTestObject: {
+        name: "Ivan",
+        age: 25,
+        hobby: "Drive cars and travel with the subway"
+    },
+    mapTestArray: [{
+            name: "Ivan",
+            age: 25,
+            hobby: "Drive cars and travel with the subway"
+        },
+        {
+            name: "Joro",
+            age: 21,
+            hobby: "Drink water"
+        },
+        {
+            name: "Mishu",
+            age: 24,
+            hobby: "streaming games"
+        },
+    ],
 }
+const getters = {
+    getField,
+    getTestMessage(state) {
+        return state.testMessage;
+    },
+    getTestObject(state) {
+        return state.testObject;
+    },
+    getTestArray(state) {
+        return state.testArray;
+    }
+}
+const mutations = {
+    updateField,
+    changeText(state, text) {
+        state.testMessage = text.target.value;
+    },
+    changeObject(state, {
+        key: key,
+        value: text
+    }) {
+        state.testObject[key] = text.target.value;
+    },
+    changeArray(state, {
+        key: key,
+        value: text,
+        index: index
+    }) {
+        state.testArray[index][key] = text.target.value;
+    }
+}
+const actions = {
+    increment: ({
+        commit
+    }) => commit('increment'),
+    decrement: ({
+        commit
+    }) => commit('decrement'),
+    incrementIfOdd({
+        commit,
+        state
+    }) {
+        if ((state.count + 1) % 2 === 0) {
+            commit('increment')
+        }
+    },
+
+}
+
 export default new Vuex.Store({
-    state
+    state,
+    getters,
+    actions,
+    mutations
 })
